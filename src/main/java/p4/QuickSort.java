1efe90aad551e5ef209dbd1ec9d9dc0e40e15957
Frank Zou <frankbearzou@gmail.com>
2023-08-27 23:16:36 -0400

    quick sort

---
+++ src/main/java/p4/QuickSort.java
@@ -0,0 +1,36 @@
+package p4;
+
+public class QuickSort {
+    public void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int pivot = partition(arr, l, r);
+            quickSort(arr, l, pivot - 1);
+            quickSort(arr, pivot + 1, r);
+        }
+    }
+
+    private int partition(int[] arr, int l, int r) {
+        int pivot = arr[r];
+        int i = l - 1;
+
+        for (int j = l; j < r; j++) {
+            if (arr[j] <= pivot) {
+                swap(arr, j, ++i);
+            }
+        }
+
+        i++;
+        swap(arr, i, r);
+        return i;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+}
---
+++ src/test/java/p4/QuickSortTest.java
@@ -0,0 +1,16 @@
+package p4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuickSortTest {
+    @Test
+    void test() {
+        int[] arr = {5,2,8,6,9,3,1,4,7};
+        QuickSort sort = new QuickSort();
+        sort.quickSort(arr);
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9}, arr);
+    }
+
+}
