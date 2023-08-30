e0f5972f3db6abb28e26e11c057e32bd9eb17023
Frank Zou <frankbearzou@gmail.com>
2023-08-30 18:59:32 -0400

    heap sort

---
+++ src/main/java/p5/MaxHeap.java
@@ -0,0 +1,51 @@
+package p5;
+
+public class MaxHeap {
+    private int size = 0;
+    int[] arr;
+
+    public void heapSort(int[] arr) {
+        // copy array
+        this.arr = arr;
+        this.size = arr.length;
+
+        for (int i = 0; i < size; i++) {
+            heapInsert(i);
+        }
+        int n = size;
+        while (n > 1) {
+            swap(0, --n);
+            heapify(0, n);
+        }
+    }
+
+    // index i go up
+    public void heapInsert(int i) {
+        // i greater than its parent
+        while (arr[i] > arr[(i - 1) / 2]) {
+            swap(i, (i - 1) / 2);
+            i = (i - 1) / 2;
+        }
+    }
+
+    // index i go down
+    public void heapify(int i, int n) {
+        int l = i * 2 + 1;
+        while (l < n) {
+            int maxIndex = l + 1 < n && arr[l + 1] > arr[l] ? l + 1 : l;
+            maxIndex = arr[i] > arr[maxIndex] ? i : maxIndex;
+            if (i == maxIndex) {
+                break;
+            }
+            swap(i, maxIndex);
+            i = maxIndex;
+            l = i * 2 + 1;
+        }
+    }
+
+    private void swap(int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+}
---
+++ src/test/java/p5/MaxHeapTest.java
@@ -0,0 +1,24 @@
+package p5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxHeapTest {
+    @Test
+    void test1() {
+        int[] arr = {1,2,3,4,5,6,7};
+        MaxHeap heap = new MaxHeap();
+        heap.heapSort(arr);
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7}, arr);
+    }
+
+    @Test
+    void test2() {
+        int[] arr = {2,4,7,5,1,6,3};
+        MaxHeap heap = new MaxHeap();
+        heap.heapSort(arr);
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7}, arr);
+    }
+
+}
